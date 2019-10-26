f52a49b39f553fd50fe8eb505b031dfe365ae8f4
Sotiris Blad <sotblad@monetaryunit.org>
2019-10-26 19:57:02 +0300

    Update CardLinks.jsx

--- client/component/Card/CardLinks.jsx
+++ client/component/Card/CardLinks.jsx
@@ -5,15 +5,13 @@ import Card from './Card';
 
 const CardLinks = () => (
   <Card title="Links">
-    <a href="https://bulwarkcrypto.com/" target="_blank" rel="nofollow noopener">Website</a><br />
-    <a href="https://bitcointalk.org/index.php?topic=2499481.0" target="_blank" rel="nofollow noopener">Bitcointalk</a><br />
-    <a href="https://github.com/bulwark-crypto" target="_blank" rel="nofollow noopener">Github</a><br />
-    <a href="https://www.reddit.com/r/BulwarkCoin/" target="_blank" rel="nofollow noopener">Reddit</a><br />
-    <a href="https://discord.gg/a7vhegP" target="_blank" rel="nofollow noopener">Discord</a><br />
-    <a href="https://t.me/bulwarkcrypto" target="_blank" rel="nofollow noopener">Telegram</a><br />
-    <a href="https://twitter.com/BulwarkCrypto" target="_blank" rel="nofollow noopener">Twitter</a><br />
-    <a href="https://medium.com/bulwark-cryptocurrency" target="_blank" rel="nofollow noopener">Medium</a><br />
-    <a href="https://community.bulwarkcrypto.com/" target="_blank" rel="nofollow noopener">Community Forum</a>
+    <a href="https://monetaryunit.org/" target="_blank" rel="nofollow noopener">Website</a><br />
+    <a href="https://bitcointalk.org/index.php?topic=778322.0" target="_blank" rel="nofollow noopener">Bitcointalk</a><br />
+    <a href="https://github.com/muecoin" target="_blank" rel="nofollow noopener">Github</a><br />
+    <a href="https://discordapp.com/invite/2jWUncm" target="_blank" rel="nofollow noopener">Discord</a><br />
+    <a href="https://twitter.com/MonetaryUnit" target="_blank" rel="nofollow noopener">Twitter</a><br />
+    <a href="https://medium.com/@MonetaryUnit" target="_blank" rel="nofollow noopener">Medium</a><br />
+    <a href="http://forum.mymue.com" target="_blank" rel="nofollow noopener">Community Forum</a>
   </Card>
 );
 
